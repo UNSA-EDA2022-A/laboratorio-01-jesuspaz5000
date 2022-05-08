326f5df5db41bf0755419211c7fff9f9ff2cf5f7
jesuspaz5000 <jpazl@gmail.com>
2022-05-08 13:26:46 -0500

    Ejercicio 2 resuelto

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -21,8 +21,47 @@ public class Exercise2 {
 	}
 
 	public Integer getMenorNumeroSaltos(int a []) {
-
-		// TO DO
+		
+		int pos = 50;
+		int saltos = 0;
+		
+		if(posible(a) == true) {
+			int i=0;
+			while(i<a.length) {
+				if(a[i] < pos) {
+					i++;
+				}
+				else if(a[i] > pos){
+					pos = a[i-1];
+					pos = pos + 50;
+					saltos++;
+				}
+				else {
+					pos = a[i];
+					pos = pos + 50;
+					saltos++;
+				}
+			}
+			if((pos-a[a.length-1]) != 50) {
+				return saltos+1;
+			}
+			else {
+				return saltos;
+			}
+		}
+		
 		return -1;
 	}
+
+	public static boolean posible(int[] posiciones) {
+		
+		for(int i=0; i<posiciones.length-1; i++) {
+			if((posiciones[i+1] - posiciones[i]) > 50) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+
 }
